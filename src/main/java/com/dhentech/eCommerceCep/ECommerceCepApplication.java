2702a6925bc08cc2e96043286e1d9d5291e94d0c
denilsonDhentech <denilson_contato@outlook.com>
2026-06-05 11:37:45 -0300

    feat: init project

---
+++ src/main/java/com/dhentech/eCommerceCep/ECommerceCepApplication.java
@@ -0,0 +1,13 @@
+package com.dhentech.eCommerceCep;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ECommerceCepApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ECommerceCepApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dhentech/eCommerceCep/ECommerceCepApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dhentech.eCommerceCep;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ECommerceCepApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
